aa5b0fccf6b5920044aeaecab40c9b57439eb3fc
sarthak gaikwad <sarthakrg14@gmail.com>
2023-03-13 16:19:00 +0530

    Connected app.js and addfetch.js to html pages

--- addfetch.js
+++ addfetch.js
@@ -1,17 +1,17 @@
-// Wait for MetaMask to be enabled
+// Wdait for MetaMask to be enable
 window.addEventListener('load', async () => {
-    // Check if MetaMask is installed
+  
     if (window.ethereum) {
-      // Request access to the user's accounts
+
       await window.ethereum.enable();
-      // Get the user's accounts
+    
       const accounts = await window.ethereum.request({ method: 'eth_accounts' });
-      // Get the first account
+ 
       const address = accounts[0];
-      // Display the address in the HTML element
+   
       document.getElementById('address').textContent = address;
     } else {
-      // If MetaMask is not installed, display an error message
+
       document.getElementById('address').textContent = 'MetaMask not installed';
     }
   });
